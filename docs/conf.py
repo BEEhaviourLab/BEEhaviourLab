7f9758286dae167efe15c8a5dc86df796200f252
Oliver King <11923466+OllyK@users.noreply.github.com>
2026-01-27 12:43:04 +0000

    docs: adds sphinx docs workflow and scaffold

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+project = "BEEhaviourLab"
+author = "BEEhaviourLab"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+exclude_patterns = []
+html_theme = "alabaster"
---
+++ docs/index.rst
@@ -0,0 +1,10 @@
+BEEhaviourLab
+=============
+
+Welcome to the BEEhaviourLab documentation.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api
