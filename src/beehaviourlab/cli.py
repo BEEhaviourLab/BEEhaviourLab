274c33072d1abb0a53ae16c4a47804ce331e1b16
Oliver King <11923466+OllyK@users.noreply.github.com>
2026-01-28 18:12:35 +0000

    feat: adds a batch processing script that processes every video in a directory tree

--- src/beehaviourlab/cli.py
+++ src/beehaviourlab/cli.py
@@ -1,6 +1,7 @@
 import click
 
 from .tracking import (
+    batch_process,
     extract_flow_info,
     fix_ids,
     process_video,
@@ -20,6 +21,7 @@ def track() -> None:
 
 
 track.add_command(process_video.main, name="run-pipeline")
+track.add_command(batch_process.main, name="batch-process")
 track.add_command(yolo_predict_to_file.main, name="run-yolo")
 track.add_command(fix_ids.main, name="fix-ids")
 track.add_command(extract_flow_info.main, name="extract-flow")
