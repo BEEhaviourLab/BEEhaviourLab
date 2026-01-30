cbbbfd4d5affea31f6355b791d0b23ada726f0c1
Oliver King <11923466+OllyK@users.noreply.github.com>
2026-01-30 11:44:21 +0000

    feat: updates the theme for docs pages

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,14 @@ author = "BEEhaviourLab"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
 ]
+autosummary_generate = True
 exclude_patterns = []
-html_theme = "alabaster"
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "show_prev_next": False,
+}
